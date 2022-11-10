28291528f82586b609e159e3c65adfda32a1ccaf
Guenfici Rayane <dracorixprod@gmail.com>
2022-11-10 10:48:49 +0100

    correction de bug

--- maquette1/X_Calculator_1_VO/connexion_traitement.php
+++ maquette1/X_Calculator_1_VO/connexion_traitement.php
@@ -1,16 +1,19 @@
 <?php
 
-$mysqli = mysqli_connect("localhost","root","");
+if((isset($_POST['Envoyer'],$_POST['login'],$_POST['mdp']))){
+    foreach ($_POST as $k => $v){
+        $$k=$v;
+    }
 
-if(!$mysqli){
-    echo "Erreur de connexion à la base de données.";
-} else {
-    $Requete = mysqli_query($mysqli,"SELECT * FROM users WHERE login = '".$_POST['login']."' AND mdp = '".md5($_POST['mdp'])."'");
+    $connexion=mysqli_connect("localhost","root","");
+    $bd=mysqli_select_db($connexion,"bd_sae");
+
+    $Requete = mysqli_query($connexion,"SELECT * FROM users WHERE login = '".$_POST['login']."' AND mdp = '".md5($_POST['mdp'])."'");
 
     if(mysqli_num_rows($Requete) == 0) {
-        header('Location: form.php?id=1');
+        header('Location: connexion.php?id=1');
         die();
-    } else {
+    } else {  
         session_start();
         $_SESSION["user"]=$_POST['login'];
         header('Location: final.php');
