30b0bebafb7e4a5798c6f69e6663c7cf1e558749
Guenfici Rayane <dracorixprod@gmail.com>
2022-11-10 10:57:41 +0100

    correctifs

--- maquette1/bd/bd_SAE.sql
+++ maquette1/bd/bd_SAE.sql
@@ -1,46 +1,29 @@
-
-drop table activiteConnexion;
-drop table activiteModule;
-drop table users;
-
-  -- A ignorer pour l'instant :
-    -- Contraintes sur type user : soit normal soit admin soit gestionnaire
-    -- Contrainte : 1 seul admine, 1 seul gestionnaire :
-        ---add constraint admin_gestion_unique check ( 
-    -- type_user = 'admin' and 'admin' not in select 
-    --)
-
 create table users(
-
-    id_user int ,
-    login char(20) unique,
-    mdp char(255) unique not null check(length(mdp)>32),
-    type_user char(20) default 'user' check (type_user in ('admin','user','gestion')) ,
-    primary key(id_user,login)
-
+    id_user int primary key,
+	
+    login char(32) not null unique,
+    mdp char(255) not null check(length(mdp)>32),
+    type_user char(32) default 'user' check (type_user in ('admin','user','gestion')),
 );
 
-
-
 create table activiteModule(
-
-    login char(20) unique,
-    id_module int not null,
+	id_activite int primary key,
+	
+	id_module int not null,
+    login char(32) not null unique,
     date_debut date not null,
     date_fin date not null,
-    primary key(login,date_debut,date_fin),
     FOREIGN KEY(login) references users(login)
 );
 
 create table activiteConnexion(
-
     id_connexion int primary key,
-    login char(20) unique,
-    reussite char not null check(reussite in ('true','false')) ,
+	
+    login char(32) unique,
+    reussite char not null check(reussite in ('true','false')),
     mdp_tente char not null,
     date_debut date not null,
     date_fin date not null,
     adr_ip char not null,
     FOREIGN KEY(login) references users(login)
-
 );
