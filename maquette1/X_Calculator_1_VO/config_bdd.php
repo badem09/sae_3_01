4155a68e9dfac20021aeb563be40df511c7dcaa2
Guenfici Rayane <dracorixprod@gmail.com>
2022-11-13 16:21:29 +0100

    Ajout de commentaire

--- maquette1/X_Calculator_1_VO/config_bdd.php
+++ maquette1/X_Calculator_1_VO/config_bdd.php
@@ -1,8 +1,9 @@
 <?php 
+    //On se connecte à la base de donnée.
     try {
         $connexion=mysqli_connect("localhost","root","");
         $bd=mysqli_select_db($connexion,"bd_sae");
-
+    //Si la connecxion a échoué, on renvoie l'erreur.
     } catch(Exception $e) {
         die('Erreur : '.$e->getMessage());
     }
