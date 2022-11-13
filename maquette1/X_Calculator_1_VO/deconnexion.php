d1c692d73358f49b0805a9dcef7545d0c55ebd7e
Guenfici Rayane <dracorixprod@gmail.com>
2022-11-13 16:25:02 +0100

    Ajout de commentaires

--- maquette1/X_Calculator_1_VO/deconnexion.php
+++ maquette1/X_Calculator_1_VO/deconnexion.php
@@ -1,6 +1,9 @@
 <?php
-
+    
+    //On démarre une session.
     session_start();
+    //Si une session "user" exixte, on renvois sur la page de connexion,
+    //on détruit la session et on ferme la page.
     if(isset($_SESSION['user'])) {
         header('Location: connexion.php');
         session_destroy();
