04d4052bb90d971e598b4b2c6704ef1f26f9ffac
Guenfici Rayane <dracorixprod@gmail.com>
2022-11-13 16:55:00 +0100

    Ajout de commentaires + modification formalisme

--- maquette1/X_Calculator_1_VO/inscription.php
+++ maquette1/X_Calculator_1_VO/inscription.php
@@ -26,6 +26,7 @@
 
                 <li class="nav-item">
                     <a href="#"><b>Mes Services</b></a>
+
                     <ul class = "nav-item-services">
                         <li><a href="connexion.php"><b>Module 1</b></a></li>
                         <li><a href="connexion.php"><b>Module 2</b></a></li>
@@ -40,65 +41,64 @@
             </ul>
         </nav>
         <div class='container-centrer'>
-            <?php echo"
-                <form action='inscription_traitement.php' method='post'>
-                    <div class='container-insciption-connexion'>
-                        <h2>S'inscrire</h2>";
-                        if(isset($_GET['err'])){
-                            switch($_GET['err']){
-                                case "mdp_non_identique" :
-                                echo "<p class='err'>Erreur : Mots de passe non identique.</p>";
-                                break;
+            <form action='inscription_traitement.php' method='post'>
+                <div class='container-insciption-connexion'>
+                    <h2>S'inscrire</h2>
+                    <?php
+                    //On verifie si une erreur à été envoyé.
+                    if(isset($_GET['err'])){
+                        //On cherche à quoi elle correspond et on l'affiche.
+                        switch($_GET['err']){
+                            case "mdp_non_identique" :
+                            echo "<p class='err'>Erreur : Mots de passe non identique.</p>";
+                            break;
 
-                                case "confirmation_vide" :
-                                echo "<p class='err>Erreur : La confirmation est vide.</p>";
-                                break;
+                            case "confirmation_vide" :
+                            echo "<p class='err>Erreur : La confirmation est vide.</p>";
+                            break;
 
-                                case "mdp_vide" :
-                                echo "<p class='err'>Erreur : Le mot de passe est vide.</p>";
-                                break;
+                            case "mdp_vide" :
+                            echo "<p class='err'>Erreur : Le mot de passe est vide.</p>";
+                            break;
 
-                                case "login_vide" :
-                                echo "<p class='err'>Erreur : Le login est vide.</p>";
-                                break;
+                            case "login_vide" :
+                            echo "<p class='err'>Erreur : Le login est vide.</p>";
+                            break;
 
-                                case "captcha_vide" :
-                                echo "<p class='err'>Erreur : Le Captcha est vide.</p>";
-                                break;
+                            case "captcha_vide" :
+                            echo "<p class='err'>Erreur : Le Captcha est vide.</p>";
+                            break;
 
-                                case "captcha_erroné" :
-                                echo "<p class='err'>Erreur : Le captcha est erroné.</p>";
-                                break;
+                            case "captcha_erroné" :
+                            echo "<p class='err'>Erreur : Le captcha est erroné.</p>";
+                            break;
 
-                                case "exist" :
-                                echo "<p class='err'>Erreur : Le nom d'utilisateur est déjà éxistant.</p>";
-                                break;
-                            }
+                            case "exist" :
+                            echo "<p class='err'>Erreur : Le nom d'utilisateur est déjà éxistant.</p>";
+                            break;
                         }
-                        echo"
-                        <div class='inputs'>
-                            <p class='titre-form'>Identifiant</p>
-                            <input type='text' name='login' placeholder='ex : demba404'/>
+                    } ?>
+                    <div class='inputs'>
+                        <p class='titre-form'>Identifiant</p>
+                        <input type='text' name='login' placeholder='ex : demba404'/>
 
-                            <p class='titre-form'>Mot de passe</p>
-                            <input type='password' name='mdp' placeholder='ex : M0t_D3_P@55€'/>
-                            
-                            <p class='titre-form'>Confirmer Mot de passe</p>
-                            <input type='password' name='mdp_retype' placeholder='ex : M0t_D3_P@55€'/>
-                            
-                            <p class='titre-form'>Captcha</p>
-                            <div class='captcha-img-desc'>
-                                <img class='img-cptcha' src='img/captcha1.png' alt='image_captcha'>
-                                <p class='captcha-desc-text'> Attention : les majuscules comptent.</p>
-                            </div>
-                            <input type='text' name='captcha' placeholder='ex : 7u5T5g3'/>
+                        <p class='titre-form'>Mot de passe</p>
+                        <input type='password' name='mdp' placeholder='ex : M0t_D3_P@55€'/>
+                        
+                        <p class='titre-form'>Confirmer Mot de passe</p>
+                        <input type='password' name='mdp_retype' placeholder='ex : M0t_D3_P@55€'/>
+                        
+                        <p class='titre-form'>Captcha</p>
+                        <div class='captcha-img-desc'>
+                            <img class='img-cptcha' src='img/captcha1.png' alt='image_captcha'>
+                            <p class='captcha-desc-text'> Attention : les majuscules comptent.</p>
                         </div>
-                        <p><input type='submit' name='Envoyer' value='S&apos;inscrire'></p>
-                        <p>Déjà inscrit ? <a id='link-sincrip' href='connexion.php'>C'est ici !</a></p>
+                        <input type='text' name='captcha' placeholder='ex : 7u5T5g3'/>
                     </div>
-                </form>
-            ";
-            ?>
+                    <p><input type='submit' name='Envoyer' value='S&apos;inscrire'></p>
+                    <p>Déjà inscrit ? <a id='link-sincrip' href='connexion.php'>C'est ici !</a></p>
+                </div>
+            </form>
         </div>
     </body>
 </html>
