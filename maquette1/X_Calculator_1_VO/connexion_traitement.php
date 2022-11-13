25ef19b9b958f0df3593014a105058535d21ead5
Guenfici Rayane <dracorixprod@gmail.com>
2022-11-13 02:38:18 +0100

    ajout erreurs + sécurité

--- maquette1/X_Calculator_1_VO/connexion_traitement.php
+++ maquette1/X_Calculator_1_VO/connexion_traitement.php
@@ -6,15 +6,24 @@
         if(!empty($_POST['login'])){
             if(!empty($_POST['mdp'])){
 
-                $Requete = mysqli_query($connexion,"SELECT * FROM users WHERE login = '".$_POST['login']."' AND mdp = '".md5($_POST['mdp'])."'");
+                $login = htmlspecialchars($_POST['login']);
+                $mdp = htmlspecialchars($_POST['mdp']);
+
+                $Requete = mysqli_query($connexion,"SELECT * FROM users WHERE login = '".$login."' AND mdp = '".md5($mdp)."'");
+                $data = $Requete->fetch_assoc();
 
                 if(mysqli_num_rows($Requete) == 0) {
-                    header('Location: connexion.php?id=1');
+                    header('Location: connexion.php?err=u_introuvable');
+                    die();
+                } elseif ($data['type_user'] !== 'user'){  
+                    session_start();
+                    $_SESSION["user"]= ["login"=>$login,"type_user"=>$data['type_user']];
+                    header('Location: page_admin.php');
                     die();
                 } else {  
                     session_start();
-                    $_SESSION["user"]=$_POST['login'];
-                    header('Location: final.php?connecte');
+                    $_SESSION["user"]= ["login"=>$login,"type_user"=>$data['type_user']];
+                    header('Location: final.php?conncté');
                     die();
                 }
             }else{
