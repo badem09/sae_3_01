139de6f660b0c90f5220f85a15460960f58c89bf
Guenfici Rayane <dracorixprod@gmail.com>
2022-11-13 16:04:18 +0100

    Ajout page admin, style, et simplification code

--- maquette1/X_Calculator_1_VO/page_admin.php
+++ maquette1/X_Calculator_1_VO/page_admin.php
@@ -49,46 +49,52 @@
             <h2>Espace administrateur</h2>
         </div>
 
-        <div class="recherche">
-            <h3>Rechercher un utilisateur</h3>
-            <form method="post"  action="">
-                <input type='text' name="text">
-                <input type='submit' name='search'>
-                <input type='submit' name='retour' value='retour'>
-            </form>
+        <div class="admin-main">
+            <div class="recherche">
+                <h3>Rechercher un utilisateur</h3>
+                <form method="post">
+                    <input id='researched' type='text' name="text"  placeholder="ex : demba404">
+                    <input id='search' type='submit' name='search' value='Recherecher'>
+                    <input id='clear' type='submit' name='retour' value='Retour'>
+                </form>
+            </div>
+
+
+            <div class="affichage">
+            <?php
+
+                require_once('config_bdd.php');
+
+                if ((isset($_POST["text"], $_POST["search"]))) {
+                    $requete2 = mysqli_query($connexion,"SELECT id_user, login, type_user from users where login='".$_POST["text"]."'");
+                    echo "<table class='tab'>";
+                    echo "<tr id='titre_tab'><th>ID Utilisateur</th><th>Nom Utilisateur</th><th>Type Utilisateur</th></tr>";
+                    while ($ligne = mysqli_fetch_row($requete2)) {
+                        echo "<tr>";
+                        foreach ($ligne as $v) { //parcours tableau de mysqli_fetch_row
+                            echo "<td>" . $v . "</td>";
+                        }
+                        echo "</tr>";
+                    }
+                    echo "</table>";
+                } elseif (isset($_POST["retour"]) ){
+                    header("location:page_admin.php");
+                } else {
+                    $requete1 = mysqli_query($connexion,"SELECT id_user, login, type_user from users");
+                    echo "<table class='tab'>";
+                    echo "<tr id='titre_tab'><th>ID Utilisateur</th><th>Nom Utilisateur</th><th>Type Utilisateur</th></tr>";
+                    while ($ligne = mysqli_fetch_row($requete1)) {
+                        echo "<tr>";
+                        foreach ($ligne as $v) { //parcours tableau de mysqli_fetch_row
+                            echo "<td>" . $v . "</td>";
+                        }
+                        echo "</tr>";
+                    }
+                    echo "</table>";
+                }
+                
+            ?>
+            </div>
         </div>
-
-        <?php
-
-            require_once('config_bdd.php');
-
-            if ((isset($_POST["text"], $_POST["search"]))) {
-                $requete2 = mysqli_query($connexion,"SELECT * from users where login='".$_POST["text"]."'");
-                echo "<table class='tab'>";
-                while ($ligne = mysqli_fetch_row($requete2)) {
-                    echo "<tr>";
-                    foreach ($ligne as $v) { //parcours tableau de mysqli_fetch_row
-                        echo "<td>" . $v . "</td>";
-                    }
-                    echo "</tr>";
-                }
-                echo "</table>";
-            } elseif (isset($_POST["retour"]) ){
-                header("location:page_admin.php");
-            } else {
-                $requete1 = mysqli_query($connexion,"SELECT * from users");
-                echo "<table class='tab'>";
-                while ($ligne = mysqli_fetch_row($requete1)) {
-                    echo "<tr>";
-                    foreach ($ligne as $v) { //parcours tableau de mysqli_fetch_row
-                        echo "<td>" . $v . "</td>";
-                    }
-                    echo "</tr>";
-                }
-                echo "</table>";
-            }
-            
-        ?>
-
     </body>
 </html>
