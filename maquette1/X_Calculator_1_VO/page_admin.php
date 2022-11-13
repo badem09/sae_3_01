626caea758f41a552190f9d8e276b9b3b4b40a17
Guenfici Rayane <dracorixprod@gmail.com>
2022-11-13 03:26:31 +0100

    clarification du code et optimisation

--- maquette1/X_Calculator_1_VO/page_admin.php
+++ maquette1/X_Calculator_1_VO/page_admin.php
@@ -52,66 +52,43 @@
         <div class="recherche">
             <h3>Rechercher un utilisateur</h3>
             <form method="post"  action="">
-                <input type='text' name="text" id="text">
-                <input type='submit' name='search' id='search' value='rechercher'>
-                <input type='submit' name='retour' id='retour' value='retour'>
+                <input type='text' name="text">
+                <input type='submit' name='search'>
+                <input type='submit' name='retour' value='retour'>
             </form>
         </div>
 
-
-
         <?php
 
-            $token = (bool)($connexion = mysqli_connect("localhost", "root", "")); // connexion avec serveur sql
+            require_once('config_bdd.php');
 
-
-            if ($token) { //vérifie connection avec serveur
-                $token2 = ($bd = mysqli_select_db($connexion, "sae_3_01")); //choisi la databse
-
-                if ($token2) { //vérifie connexion avec database
-                    $table = "activitemodule"; //choix table
-
-                    if ((isset($_POST["text"], $_POST["search"]))) { //si recherche d'un login, alors on doit mettre a jour requete sql avec élément recherche
-                        $utilsateur = $_POST["text"];
-                        $requete = "SELECT * from $table where login='$utilsateur'"; //Requete SQL affichant le tableau avec l'utilisateur choisi et statistiques
-
-                        $token3 = (bool)($res = mysqli_query($connexion, $requete));
-                        if ($token3) { //vérifie connexion entre mysql et requête
-                            echo "<table class='tab'>";
-                            while ($ligne = mysqli_fetch_row($res)) {
-                                echo "<tr>";
-                                foreach ($ligne as $v) { //parcours tableau de mysqli_fetch_row
-                                    echo "<td>" . $v . "</td>";
-                                }
-                                echo "</tr>";
-                            }
-                            echo "</table>";
-                        }
+            if ((isset($_POST["text"], $_POST["search"]))) {
+                $requete2 = mysqli_query($connexion,"SELECT * from users where login='".$_POST["text"]."'");
+                echo "<table class='tab'>";
+                while ($ligne = mysqli_fetch_row($requete2)) {
+                    echo "<tr>";
+                    foreach ($ligne as $v) { //parcours tableau de mysqli_fetch_row
+                        echo "<td>" . $v . "</td>";
                     }
-                    elseif ( isset($_POST["retour"]) ){
-                        header("location:page_admin.php");
-                    }
-
-                    else {  // si pas de recherche, alors affiche liste tout les utilisateurs
-                        $requete = "SELECT * from $table";
-                        $token3 = (bool)($res = mysqli_query($connexion, $requete));
-
-                        $token3 = (bool)($res = mysqli_query($connexion, $requete));
-                        if ($token3) { //vérifie connexion entre mysql et requête
-                            echo "<table class='tab'>";
-                            while ($ligne = mysqli_fetch_row($res)) {
-                                echo "<tr>";
-                                foreach ($ligne as $v) { //parcours tableau de mysqli_fetch_row
-                                    echo "<td>" . $v . "</td>";
-                                }
-                                echo "</tr>";
-                            }
-                            echo "</table>";
-                        }
-                    }
+                    echo "</tr>";
                 }
+                echo "</table>";
+            } elseif (isset($_POST["retour"]) ){
+                header("location:page_admin.php");
+            } else {
+                $requete1 = mysqli_query($connexion,"SELECT * from users");
+                echo "<table class='tab'>";
+                while ($ligne = mysqli_fetch_row($requete1)) {
+                    echo "<tr>";
+                    foreach ($ligne as $v) { //parcours tableau de mysqli_fetch_row
+                        echo "<td>" . $v . "</td>";
+                    }
+                    echo "</tr>";
+                }
+                echo "</table>";
             }
+            
         ?>
 
-</body>
+    </body>
 </html>
