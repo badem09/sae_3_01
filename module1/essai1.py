83d1b540e2116c1121a45ddc8dfbb6edb5310940
BA Demba <badembajob@gmail.com>
2022-11-22 18:41:45 +0100

    bug : reprendre essai2.py

--- module1/essai1.py
+++ module1/essai1.py
@@ -35,7 +35,7 @@ def loi_normale(x,esp,et):
 
     return res
 
-def methode_rectangle_gauche(esp,et,b,a=0):
+def methode_rectangle_gauche(esp,et,b,a):
     sum = 0
     n = ((b - a) ** 2 ) ** 0.5 
     for i in range(a,b):
@@ -96,51 +96,4 @@ def methode_Simpson(esp,et,b,a=0):
 x = [0,10,20]
 p = [1/4, 1/2, 1/4]
 
-#print(esperance(x,p))
-#print(loi_normale(15, esperance(x,p), ecart_type(x,p)))
-
-"""print("rectangle gauche : " ,methode_rectangle_gauche(10,50,25))
-print("rectangle droits : " , methode_rectangle_droite(10,50,25))
-print("rectangle medians : " , methode_rectangle_medians(10,50,25))
-print("trapèzes : " , methode_trapeze(10,50,25))
-print("simpson : " , methode_Simpson(10,50,25))"""
-
-#Pour les tests :
-#site reference : https://www.codabrainy.com/loi-normale/10/50/0/25/ 
-
-
-
-## Ce que je peux faire c'est je regarde manuellement ou la courbe
-## devient asymptote (coté négatif) et je considère que c'est min 
-## donc que de min à -inf ya que des 0 (algo de recherche dichotomie?)
-
-# si = 0.9999999999999991 =1
-
-
-
-## Chercher la solution en 0 de la dérivé de la loi normale fait buger 
-## les sorties changent et ne sont plus cohérentes
-
-#esp= Symbol('esp')
-#et= Symbol('et')
-#print(diff(loi_normale(x,10,50))) # dérivé
-#an = Symbol('an')
-#equation = loi_normale(x,50,10)
-#y = Symbol('y')
-#print(solve(diff(loi_normale(y,10,50))))
-"""
-x= Symbol('x')
-f= 1/  (2 * pi)**0.5 * exp(-(x**2)/2 )
-print(integrate(f,(x,-inf,2)))"""
-
-#print("rectangle gauche : " ,methode_rectangle_gauche(1,1,2,-100000))
-
-def f(x):
-   return 1/  (2 * pi)**0.5 * exp(-(x**2)/2 )
-
-from matplotlib.pyplot import plot, show, legend
-x= Symbol('x')
-ab = [ i for i in range(-10,10) ]
-ord = [ f(a) for a in range(-10,10) ]
-plot(ab,ord,color='orange')
-show()
+print("rectangle gauche : " ,methode_rectangle_gauche(90,3,91,85))
