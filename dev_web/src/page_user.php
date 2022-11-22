b7de0f013cc0ffdf698419ee2ceb723ad1e70b44
Guenfici Rayane <dracorixprod@gmail.com>
2022-11-22 17:44:11 +0100

    mise en place du formulaire changer dmp

--- dev_web/src/page_user.php
+++ dev_web/src/page_user.php
@@ -51,14 +51,74 @@
 			</ul>
 		</nav>
 
-		<div class="erreur">
-			<h1>Inscription ou connexion réussie !</h1>
-			<!--On récupere le login qui se trouve dans la session et on l'affiche.-->
-			<h3>Bonjour <?php echo $_SESSION['user']['login'];?> !</h3>
-			<p>
-				<br>Cette page est en cours de développement !
-			</p>
+    <div class="entete">
+        <h1>X Calculator</h1>
+        <h3>Bonjour <?php echo $_SESSION['user']['login'];?> !</h3>
+    </div>
+
+		<div class='container-centrer'>
+			<form action='inscription_traitement.php' method='post'>
+	        <div class='container-insciption-connexion'>
+	            <h2>Modifier mon MDP</h2>
+	            <?php
+	            //On verifie si une erreur à été envoyé.
+	            if(isset($_GET['err'])){
+	                //On cherche à quoi elle correspond et on l'affiche.
+	                switch($_GET['err']){
+	                    case "mdp_non_identique" :
+	                    echo "<p class='err'>Erreur : Mots de passe non identique.</p>";
+	                    break;
+
+	                    case "confirmation_vide" :
+	                    echo "<p class='err>Erreur : La confirmation est vide.</p>";
+	                    break;
+
+	                    case "mdp_vide" :
+	                    echo "<p class='err'>Erreur : Le mot de passe est vide.</p>";
+	                    break;
+
+	                    case "login_vide" :
+	                    echo "<p class='err'>Erreur : Le login est vide.</p>";
+	                    break;
+
+	                    case "captcha_vide" :
+	                    echo "<p class='err'>Erreur : Le Captcha est vide.</p>";
+	                    break;
+
+	                    case "captcha_erroné" :
+	                    echo "<p class='err'>Erreur : Le captcha est erroné.</p>";
+	                    break;
+
+	                    case "exist" :
+	                    echo "<p class='err'>Erreur : Nom d'utilisateur déjà éxistant.</p>";
+	                    break;
+	                }
+	            } ?>
+	            <div class='inputs'>
+	            		<p class='titre-form'>Mot de passe</p>
+	                <label id="label-register-password" for="register-password">
+	                <input id='register-password' type='password' name='a_mdp' placeholder='ex : M0t_D3_P@55€'/>
+
+	                <p class='titre-form'>Mot de passe</p>
+	                <label id="label-register-password" for="register-password">
+	                <input id='register-password' type='password' name='mdp' placeholder='ex : N0Uv€@U_M0t_D3_P@55€'/>
+	                
+	                <p class='titre-form'>Confirmer Mot de passe</p>
+	                <label id="label-register-password-2" for="register-password-2">
+	                <input id='register-password-2' type='password' name='mdp_retype' placeholder='ex : N0Uv€@U_M0t_D3_P@55€'/>
+	                
+	                <p class='titre-form'>Captcha</p>
+	                <div class='captcha-img-desc'>
+	                    <img class='img-cptcha' src='img/captcha1.png' alt='image_captcha'>
+	                    <p class='captcha-desc-text'> Attention : les majuscules comptent.</p>
+	                </div>
+	                <label id="label-register-captcha" for="register-captcha">
+	                <input id='register-captcha' type='text' name='captcha' placeholder='ex : 7u5T5g3'/>
+	                <p><input type='submit' name='Envoyer' value='Modifier'></p>
+	            </div>
+	        </div>
+	    </form>
 		</div>
-	
+
   </body>
 </html>
