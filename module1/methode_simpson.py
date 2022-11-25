1487cceb6c0352eb22d635066468f76df49550e6
Suban.S <sasikumarsuban2@gmail.com>
2022-11-25 19:30:46 +0100

    spécification de la fonction

--- module1/methode_simpson.py
+++ module1/methode_simpson.py
@@ -4,6 +4,18 @@ import sys
 
 
 def methode_simpson(m, et, t,n):
+    """ 
+    Calcul de l'intégrale correspondant à P(X<t) avec 
+    la méthode de Simpson
+    Entrées:
+            m : éspérance (float / int)
+            et : écart-type (float / int)
+            t : variable (float / int)
+            n : nombre de divisions/rectangles (int)
+    Retour:
+            Résultat (float)
+
+    """
     return fi.methode_simpson(m, et, t,n)
 
 #print(sys.argv)
