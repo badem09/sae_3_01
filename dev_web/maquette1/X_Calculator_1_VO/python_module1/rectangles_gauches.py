cb20d61b0610aba21ec7b889cc305b0f26245b73
BA Demba <badembajob@gmail.com>
2022-11-25 21:40:09 +0100

    Copie des script python dans le dossier dev_web

---
+++ dev_web/maquette1/X_Calculator_1_VO/python_module1/rectangles_gauches.py
@@ -0,0 +1,33 @@
+import fonctions_integrales as fi
+import sys
+
+def rectangles_gauches(m, et, t,n):
+    """ 
+    Calcul de l'intégrale correspondant à P(X<t) avec 
+    la méthode des rectangles gauches
+    Entrées:
+            m : éspérance (float / int)
+            et : écart-type (float / int)
+            t : variable (float / int)
+            n : nombre de divisions/rectangles (int)
+    Retour:
+            Résultat (float)
+
+    """
+    return fi.methode_rectangle_gauche(m, et, t,n)
+
+#print(sys.argv)
+# faire des tests pour que 
+try:
+    m = float(sys.argv[1])
+    et = float(sys.argv[2])
+    t = float(sys.argv[3])
+    n = 1000000
+
+    print(round(rectangles_gauches(m,et,t,n),5))
+
+except:
+    print("L'une des valeurs rentrée n'est pas au bon format")
+
+
+
