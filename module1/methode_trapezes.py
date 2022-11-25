ac07bc482e7cb44071c015a694f4c24f83ebe42c
Suban.S <sasikumarsuban2@gmail.com>
2022-11-25 19:30:57 +0100

    spécification de la fonction

--- module1/methode_trapezes.py
+++ module1/methode_trapezes.py
@@ -3,6 +3,18 @@ import sys
 
 
 def methode_trapeze(m, et, t,n):
+    """ 
+    Calcul de l'intégrale correspondant à P(X<t) avec 
+    la méthode des trapèzes
+    Entrées:
+            m : éspérance (float / int)
+            et : écart-type (float / int)
+            t : variable (float / int)
+            n : nombre de divisions/rectangles (int)
+    Retour:
+            Résultat (float)
+
+    """
     return fi.methode_trapeze(m, et, t,n)
 
 #print(sys.argv)
