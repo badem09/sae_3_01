c5684a89a66e3a4a2bd35a6998d55cc0d582637d
Guenfici Rayane <dracorixprod@gmail.com>
2022-11-29 13:53:42 +0100

    ajout module 1

--- dev_web/src/module1.php
+++ dev_web/src/module1.php
@@ -1,3 +1,14 @@
+<?php
+
+    //On regarde si une cession existe.
+    session_start();
+    //Si aucune cession existe, on renvois sur la page de connexion.
+    if(!isset($_SESSION['user'])) {
+       header('Location: connexion.php');
+    }
+ 
+?>
+
 <!doctype html>
 <html lang="fr">
   <head>
@@ -33,68 +44,119 @@
 					</ul>
 				</li>
 
-				<li class="nav-item">
-					<a href="connexion.php"><b>Se connecter</b></a>
-				</li>
+				<?php
+
+                    //On regarde si une cession existe.
+                    session_start();   
+                    //Si aucune cession existe, on renvois sur la page de connexion.
+                    if(isset($_SESSION['user'])) {
+                        if($_SESSION['user']['type_user'] != 'user'){
+                            echo"
+                            <li class='nav-item'>
+                              <a href='page_admin.php'><b>Administration</b></a>
+                            </li>
+
+                            <li class='nav-item'>
+                                <a href='page_user.php'><b>Mon Espace</b></a>
+                            </li>
+
+                            <li class='nav-item'>
+                                <a href='deconnexion.php'><b>Se déconnecter</b></a>
+                            </li>
+                            ";
+                        }else{
+                            echo "
+                            <li class='nav-item'>
+                              <a href='page_user.php'><b>Mon Espace</b></a>
+                            </li>
+
+                            <li class='nav-item'>
+                                <a href='deconnexion.php'><b>Se déconnecter</b></a>
+                            </li>
+                            ";
+                        }
+                    }else{
+                        echo "
+                        <li class='nav-item'>
+                            <a href='connexion.php'><b>Se connecter</b></a>
+                        </li>
+                        ";
+                    }
+                    
+                ?>
 
 			</ul>
 		</nav>
 
-		<div class="formulaire">
-			<h1>Calcul d'une probabilité à l'aide d'une loi normale</h1>
-			
-			<form name="form_module1" action="module1.php" method="post">
-                <select name="choix_methode" id="choix_methode">
-                    <option value="rectangles_gauches.py">Rectangles gauches</option>
-                    <option value="rectangles_droites.py">Rectangles droits</option>
-                    <option value="rectangles_medians.py">Rectangles medians</option>
-                    <option value="methode_trapezes.py">Trapèzes</option>
-                    <option value="methode_simpson.py">Simpson</option>
-            
-                </select>
-                <p>
-                    Valeur de µ :<br />
-                    <input type="text" name="esp" value="" />
-                </p>
-                <p>
-                    Valeur de σ :<br />
-                    <input type="text" name="et" value=""  />
-                </p>
-                <p>
-                    Valeur de t :<br />
-                    <input type="text" name="t" value=""  />
-                </p>
-                <p>
-                    <input type="submit" value="Calculer P( t < X ) " />
-                    <input type="reset" value="Annuler" />
-                </p>
+        <div class="entete">
+            <h1>X Calculator</h1>
+            <h2>Module 1</h2>
+            <br>
+            <p class="pacc_mod_pres">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </div>
+
+        <div class='container-centrer'>
+            <form name="form_module1" action='module1.php' method='post'>
+                <div class='container-module'>
+                    <div class='container-form'>
+                        <h2>Module 1</h2>
+                        <h3>Calcul d'une probabilité à l'aide d'une loi normale</h3>
+
+                        <select name="choix_methode" id="choix_methode">
+                            <option value="rectangles_gauches.py">Rectangles gauches</option>
+                            <option value="rectangles_droites.py">Rectangles droits</option>
+                            <option value="rectangles_medians.py">Rectangles medians</option>
+                            <option value="methode_trapezes.py">Trapèzes</option>
+                            <option value="methode_simpson.py">Simpson</option>
+                        </select>
+
+                        <div class='inputs'>
+
+                            <p>Valeur de µ :</p>
+                            <input type="text" name="esp" value=""/>
+
+                            <p>Valeur de σ :</p>
+                            <input type="text" name="et" value=""/>
+
+                            <p>Valeur de t :</p>
+                            <input type="text" name="t" value=""/>
+
+                        </div>
+
+                        <p><input id="label-login-mdp" type="submit" value="Calculer P( t < X ) "/></p>
+                        <p><input id="label-login-mdp" type="reset" value="Annuler"/></p>
+                    </div>
+                    <div class='container-resultat'>
+                        <h2>Résultat</h2>
+
+                        <?php
+
+                            if (isset($_POST['esp'], $_POST['et'], $_POST['t'])){
+                                $esp = $_POST["esp"]; #récupere les entrées
+                                $et = $_POST["et"] ;
+                                $t = $_POST["t"];
+                                $fonction = $_POST["choix_methode"] ;
+
+                                $command = 'python'. ' '. 'python_module1/' . $fonction . ' '
+                                    . $esp . ' ' . $et . ' ' .$t . ' ' ; # Préparation de la commande 
+
+                                $result = shell_exec($command); # execution de la commande dans un shell 'imaginaire'
+                                                                # et on récupere le resultat
+                                $float_value = (float) $result; #conversion du resultat en float
+                                if (strval($float_value) == $result){ # si le résultat est bien un nb a virgule (float)
+                                    echo 'P( ' . $t . ' < X ) = ' . $result;
+                                }
+                                else{ #sinon erreur fournie par script python
+                                    echo '<h2>'.$result.'</h2>';
+                                }
+
+                            }
+                            ?>
+
+                    </div>
+                </div>
             </form>
-		</div>
+        </div>
 	
   </body>
 </html>
-
-<?php
-
-if (isset($_POST['esp'], $_POST['et'], $_POST['t'])){
-    $esp = $_POST["esp"]; #récupere les entrées
-    $et = $_POST["et"] ;
-    $t = $_POST["t"];
-    $fonction = $_POST["choix_methode"] ;
-
-    $command = 'python'. ' '. 'python_module1/' . $fonction . ' '
-        . $esp . ' ' . $et . ' ' .$t . ' ' ; # Préparation de la commande 
-
-    $result = shell_exec($command); # execution de la commande dans un shell 'imaginaire'
-                                    # et on récupere le resultat
-    $float_value = (float) $result; #conversion du resultat en float
-    if (strval($float_value) == $result){ # si le résultat est bien un nb a virgule (float)
-        echo 'P( ' . $t . ' < X ) = ' . $result;
-    }
-    else{ #sinon erreur fournie par script python
-        echo $result;
-    }
-
-}
-?>
-
