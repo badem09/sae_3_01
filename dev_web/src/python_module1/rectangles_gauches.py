67cd1b6bdf616488d60cdc4662b9acfec0e17916
BA Demba <badembajob@gmail.com>
2022-12-02 20:18:32 +0100

    PAS STABLE  : Ajout de la création d'un fichier pour recencer les erreur

--- dev_web/src/python_module1/rectangles_gauches.py
+++ dev_web/src/python_module1/rectangles_gauches.py
@@ -1,5 +1,5 @@
 import fonctions_integrales as fi
-import sys
+import sys , platform, subprocess, traceback
 
 def rectangles_gauches(m, et, t,n):
     """ 
@@ -16,19 +16,31 @@ def rectangles_gauches(m, et, t,n):
     """
     return fi.methode_rectangle_gauche(m, et, t,n)
 
-#print(sys.argv)
-# faire des tests pour que 
-try:
-    m = float(sys.argv[1])
-    et = float(sys.argv[2])
-    t = sys.argv[3]
-    n = 1000
-    retour = str(rectangles_gauches(m,et,float(t),n))
-    print("P(X<" + t +  ") = " +  retour[:9])
 
 
-except:
-    print("L'une des valeurs rentrée n'est pas au bon format")
+#try:
+m = float(sys.argv[1])
+et = float(sys.argv[2])
+t = sys.argv[3]
+n = 1000
+retour = str(rectangles_gauches(m,et,float(t),n))
+print("P(X<" + t +  ") = " +  retour[:9])
+    
+
+
+""" A tester sur le raspbery
+except Exception as e :
+    f = open("fichierErrors.txt","w")
+    if platform.system().casefold() == 'Windows'.casefold():
+        date = subprocess.getoutput("echo %date% %time%")
+   
+    elif platform.system().casefold() == 'Linux'.casefold():
+        date = subprocess.getoutput("date")
+    
+    f.write(date + "\n\n" + traceback.format_exc() + "\n" + str(sys.argv))
+
+    f.close()"""
+
 
 
 
