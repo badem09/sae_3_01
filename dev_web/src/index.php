741d50691728278f7b03a7a51d3d648c91ec2a0b
BA Demba <badembajob@gmail.com>
2022-12-03 16:11:06 +0100

    Correction redirection et ajout texte decriptif et image du module 1

--- dev_web/src/index.php
+++ dev_web/src/index.php
@@ -20,11 +20,10 @@
 
 				<li class="nav-item">
 					<a href="#"><b>Mes Services</b></a>
-					
 					<ul class = "nav-item-services">
-						<li><a href="connexion.php"><b>Module 1</b></a></li>
-						<li><a href="connexion.php"><b>Module 2</b></a></li>
-						<li><a href="connexion.php"><b>Module 3</b></a></li>
+						<li><a href="module1.php"><b>Module 1</b></a></li>
+						<li><a href="404.php"><b>Module 2</b></a></li>
+						<li><a href="404.php"><b>Module 3</b></a></li>
 					</ul>
 				</li>
 
@@ -76,37 +75,38 @@
 			<h1>X Calculator</h1>
 			<h2>On peut tout calculer !</h2>
 			<br>
-			<p class="pacc_mod_pres">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
 		</div>
 
 		<div class="presentation">
-			<h2>Texte explicatif</h2>
-			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec egestas velit ex, vel sodales tellus commodo non. Vivamus aliquet metus vel purus semper tristique. Proin ipsum justo, aliquam nec faucibus nec, bibendum nec turpis.</p>
+			<h2>Bienvenue !</h2>
 		</div>
 
 		<div class="pacc_mod_gauche"> 
 			<div class="pacc_mod_image">
-				<img src="img\carre_page_acceuil.png" alt="image_EnCoursDeDeveloppemnt">
+				<img src="img\module1.png" alt="image d'une courbe d'une fonction mathématique quelquonque">
 			</div>
 
 			<div>
-				<h3>Module1</h3>
+				<h3>Module de probabilité : calcul approché d'une loi normale.</h3>
 
-				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec egestas velit ex, vel sodales tellus commodo non. Vivamus aliquet metus vel purus semper tristique. Proin ipsum justo, aliquam nec faucibus nec, bibendum nec turpis.</p>
+				<p>Ce module vous permettra de de calculer une probabilité 
+					dans le cadre d’une loi normale de paramètres m et σ.</br>
+					Vous pourrez calculer P (X < t) en saisissant la valeur de m, de σ et t
+					</br>
+					Vous aurez le choix entre les méthodes des rectangles, 
+					la méthode des trapèzes et la méthode de Simpson.
+				</p>
 
-				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec egestas velit ex, vel sodales tellus commodo non. Vivamus aliquet metus vel purus semper tristique. Proin ipsum justo, aliquam nec faucibus nec, bibendum nec turpis.</p>
-				<a href="connexion.php" aria-label="lien_page_connexion"><input type="button" value="Y aller"></a>
+				<a href="module1.php" aria-label="lien_page_connexion"><input type="button" value="Y aller"></a>
 			</div>
 		</div>
 
 		<div class="pacc_mod_droit"> 
 			<div>
 				<h3>Module2</h3>
-
-				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec egestas velit ex, vel sodales tellus commodo non. Vivamus aliquet metus vel purus semper tristique. Proin ipsum justo, aliquam nec faucibus nec, bibendum nec turpis.</p>
-
-				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec egestas velit ex, vel sodales tellus commodo non. Vivamus aliquet metus vel purus semper tristique. Proin ipsum justo, aliquam nec faucibus nec, bibendum nec turpis.</p>
-				<a href="connexion.php" aria-label="lien_page_connexion"><input type="button" value="Y aller"></a>
+				<p>A venir très prochainement !</p>
+				</br></br></br></br></br></br></br>
+				<a href="404.html" aria-label="lien_page_connexion"><input type="button" value="Y aller"></a>
 			</div>
 
 			<div class="pacc_mod_image">
@@ -120,12 +120,11 @@
 			</div>
 
 			<div>
-				<h3>Module1</h3>
+				<h3>Module3</h3>
 
-				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec egestas velit ex, vel sodales tellus commodo non. Vivamus aliquet metus vel purus semper tristique. Proin ipsum justo, aliquam nec faucibus nec, bibendum nec turpis.</p>
-
-				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec egestas velit ex, vel sodales tellus commodo non. Vivamus aliquet metus vel purus semper tristique. Proin ipsum justo, aliquam nec faucibus nec, bibendum nec turpis.</p>
-				<a href="connexion.php" aria-label="lien_page_connexion"><input type="button" value="Y aller"></a>
+				<p>A venir très prochainement !</p>
+				</br></br></br></br></br></br></br>
+				<a href="404.html" aria-label="lien_page_connexion"><input type="button" value="Y aller"></a>
 			</div>
 		</div>
 
