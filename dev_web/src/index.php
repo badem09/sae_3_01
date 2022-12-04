ba4955c81a2d71e56243bd2a7257d6c5a1e9fb86
Guenfici Rayane <dracorixprod@gmail.com>
2022-12-04 19:08:05 +0100

    generalisation navbar

--- dev_web/src/index.php
+++ dev_web/src/index.php
@@ -7,69 +7,11 @@
   ?>
   
   <body>
-		<nav class = "menu-nav">
-			<ul id="ul-container">
-
-				<li class ="nav-logo">
-					<a href="#"><img class ="nav-logo" src="img\logo_t.png" alt="logo_SiteWeb_X"></a>
-				</li>
-
-				<li class="nav-item">
-					<a href="#"><b>Page d'accueil</b></a>
-				</li>
-
-				<li class="nav-item">
-					<a href="#"><b>Mes Services</b></a>
-					<ul class = "nav-item-services">
-						<li><a href="module1.php"><b>Module 1</b></a></li>
-						<li><a href="404.php"><b>Module 2</b></a></li>
-						<li><a href="404.php"><b>Module 3</b></a></li>
-					</ul>
-				</li>
-
-				<?php
-
-					//On regarde si une cession existe.
-          session_start();	 
-			    //On attribus les bon boutons au bonnes personnes.
-			    if(isset($_SESSION['user'])) {
-				    if($_SESSION['user']['type_user'] != 'user'){
-				        echo"
-				        <li class='nav-item'>
-                  <a href='page_admin.php'><b>Administration</b></a>
-                </li>
-
-                <li class='nav-item'>
-	              	<a href='page_user.php'><b>Mon Espace</b></a>
-	            	</li>
-
-                <li class='nav-item'>
-									<a href='deconnexion.php'><b>Se déconnecter</b></a>
-								</li>
-                ";
-			      }else{
-		        	echo "
-			        <li class='nav-item'>
-	              <a href='page_user.php'><b>Mon Espace</b></a>
-	            </li>
-
-	            <li class='nav-item'>
-								<a href='deconnexion.php'><b>Se déconnecter</b></a>
-							</li>
-		        	";
-		        }
-			    }else{
-	        	echo "
-						<li class='nav-item'>
-							<a href='connexion.php'><b>Se connecter</b></a>
-						</li>
-	        	";
-	        }
-				 
-				?>
-
-			</ul>
-		</nav>
+		
+		<?php
+	  	//On inclus la barre de navigation.
+	  	require("imports_html/nav_bar.html");
+  	?>
 
 		<div class="entete">
 			<h1>X Calculator</h1>
