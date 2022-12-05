1bdd39ea62884cddf301e479c643ad997472ec61
Suban.S <sasikumarsuban2@gmail.com>
2022-12-05 19:46:05 +0100

    Debut code chiffrement

---
+++ module2/crytpo.py
@@ -0,0 +1,53 @@
+import numpy as np
+
+
+def KSA (cle) :
+    
+    longueur_cle = len(cle)
+    S = list(range(2056))
+    j = 0
+    for i in range (256):
+        j = (j + S[i] + cle[i%longeur_cle]) % 256
+        S[i], S[j] = S[j], S[i]
+    return S
+
+
+
+
+def PRGA (S,n) :
+    i = 0
+    j = 0 
+    key=[]
+    
+    while n>0:
+        n=n-1
+        i = (i+1) % 256
+        j = (j+S[i]) % 256
+        S[i], S[j] = S[j], S[i]
+        K = S[S[i] + S[j] %256]
+        key.append(K)
+        
+    return key    
+
+
+
+def preparing_key_array(s) :
+    return (ord(c) for c in s)
+
+
+
+key = "Demba"
+text = "Ba"
+key = preparing_key_array(key)
+
+S = KSA(key)
+
+keystream = np.array(PRGA(S,len(text)))
+print(keystream)
+
+text = np.array([ord(i) for i in text])
+
+cipher = keystream^text
+
+print(cipher.astype(np.unit8).data.hex())
+print( (chr(c) for c in cipher))
