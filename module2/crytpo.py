a4e6611ad765dc57362e9834604c0abfaaffd66b
Demba BA <badembajob@gmail.com>
2022-12-05 20:06:07 +0100

    Correction typo

--- module2/crytpo.py
+++ module2/crytpo.py
@@ -4,10 +4,10 @@ import numpy as np
 def KSA (cle) :
     
     longueur_cle = len(cle)
-    S = list(range(2056))
+    S = list(range(256))
     j = 0
     for i in range (256):
-        j = (j + S[i] + cle[i%longeur_cle]) % 256
+        j = (j + S[i] + cle[i%longueur_cle]) % 256
         S[i], S[j] = S[j], S[i]
     return S
 
@@ -24,20 +24,18 @@ def PRGA (S,n) :
         i = (i+1) % 256
         j = (j+S[i]) % 256
         S[i], S[j] = S[j], S[i]
-        K = S[S[i] + S[j] %256]
+        K = S[(S[i] + S[j]) %256]
         key.append(K)
         
     return key    
 
+key = "KAREEM"
+text = "Mission Accomplished"
 
 
 def preparing_key_array(s) :
-    return (ord(c) for c in s)
+    return [ord(c) for c in s]
 
-
-
-key = "Demba"
-text = "Ba"
 key = preparing_key_array(key)
 
 S = KSA(key)
@@ -47,7 +45,7 @@ print(keystream)
 
 text = np.array([ord(i) for i in text])
 
-cipher = keystream^text
+cipher = keystream ^ text
 
-print(cipher.astype(np.unit8).data.hex())
-print( (chr(c) for c in cipher))
+print(cipher.astype(np.uint8).data.hex())
+print( [chr(c) for c in cipher])
