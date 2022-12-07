8a059502e97ae1a23eeeb172467764537231f6fd
Demba BA <badembajob@gmail.com>
2022-12-07 18:48:51 +0100

    Copie du rep /module1 pour tester module2.php

---
+++ module2/page_admin.php
@@ -0,0 +1,99 @@
+<?php
+
+    //On démarre une cession existe.
+    session_start();
+    //On vérifie si une cession existe.
+    if(isset($_SESSION['user'])) {
+        //Si c'est un simple utilisateur, on renvois sur la page de connexion.
+        if($_SESSION['user']['type_user'] == 'user'){
+            header('Location: connexion.php');
+            die();
+        }
+    }else{
+        header('Location: connexion.php');
+        die();
+    }
+
+?>
+
+<!doctype html>
+<html lang="fr">
+    
+    <?php
+        //On inclus le header de la page.
+        require("imports_html/head.html");
+    ?>
+
+    <body>
+        
+        <?php
+            //On inclus la barre de navigation.
+            require("imports_html/nav_bar.html");
+        ?>
+
+        <div class="entete">
+            <h1>X Calculator</h1>
+            <h2>Espace administrateur</h2>
+        </div>
+
+        <div class="admin-main">
+            <div class="recherche">
+                <h3>Rechercher un utilisateur</h3>
+
+                <form method="post">
+                    <input id='researched' type='text' name="text"  placeholder="ex : demba404">
+                    <input id='search' type='submit' name='search' value='Recherecher'>
+                    <input id='clear' type='submit' name='retour' value='Retour'>
+                </form>
+            </div>
+
+            <div class="affichage">
+            <?php
+                //On inclus la configuration d'accès à la base de donnée avant de commencer.
+                require_once('config/config_bdd.php');
+                //Si une recherche a été demandé.
+                if ((isset($_POST["text"], $_POST["search"]))) {
+                    //On récupere de la base de donnée "id_user", "login", "type_user" si un utilisateur correspond à l'entrée.
+                    $requete2 = mysqli_query($connexion,"SELECT id_user, login, type_user from users where login like '".$_POST["text"]."%'");
+                    //On affiche la base d'un tableau.
+                    echo "<table class='tab'>";
+                    //On affiche les titres du tableau.
+                    echo "<tr id='titre_tab'><th>ID Utilisateur</th><th>Nom Utilisateur</th><th>Type Utilisateur</th></tr>";
+                    //Pour chaques lignes assigé comme plusieurs valeurs, on récupère et affiche les données (1 seul car chaque "login" est unique).
+                    while ($ligne = mysqli_fetch_row($requete2)) {
+                        echo "<tr>";
+                        foreach ($ligne as $v) {
+                            echo "<td>" . $v . "</td>";
+                        }
+                        echo "</tr>";
+                    }
+                    echo "</table>";
+                //Si un retour à été demandé, on redirige vers la meme page pour actualiser et on ferme celle-ci.
+                } elseif (isset($_POST["retour"]) ){
+                    header("location:page_admin.php");
+                    die();
+                //Si aucune action n'a été effectué on affiche toutes les données présente dans la base de donnée.
+                //(Dans le cas ou nous arrivons sur la page, c'est cette condition sui est utiliser pour afficher les données sans avoir à les demander.)
+                } else {
+                    $requete1 = mysqli_query($connexion,"SELECT id_user, login, type_user from users");
+                    //On affiche la base d'un tableau.
+                    echo "<table class='tab'>";
+                    //On affiche les titres du tableau.
+                    echo "<tr id='titre_tab'><th>ID Utilisateur</th><th>Nom Utilisateur</th><th>Type Utilisateur</th></tr>";
+                    //Pour chaques lignes assigé comme plusieurs valeurs, on récupère et affiche les données (1 seul car chaque "login" est unique).
+                    while ($ligne = mysqli_fetch_row($requete1)) {
+                        echo "<tr>";
+                        foreach ($ligne as $v) { //parcours tableau de mysqli_fetch_row
+                            echo "<td>" . $v . "</td>";
+                        }
+                        echo "</tr>";
+                    }
+                    echo "</table>";
+                }
+                
+            ?>
+            </div>
+        </div>
+    
+    </body>
+</html>
---
+++ module2/page_user.php
@@ -0,0 +1,111 @@
+<?php
+
+	//On regarde si une cession existe.
+	session_start();
+	//Si aucune cession existe, on renvois sur la page de connexion.
+	if(!isset($_SESSION['user'])) {
+	   header('Location: connexion.php');
+	}
+ 
+?>
+
+<!doctype html>
+<html lang="fr">
+
+  <?php
+    //On inclus le header de la page.
+    require("imports_html/head.html");
+  ?>
+  
+  <body>
+		
+		<?php
+	  	//On inclus la barre de navigation.
+	  	require("imports_html/nav_bar.html");
+  	?>
+
+    <div class="entete">
+        <h1>X Calculator</h1>
+        <h3>Bonjour <?php echo $_SESSION['user']['login'];?> !</h3>
+    </div>
+
+		<div class='container-centrer'>
+			<form action='modif_mdp_traitement.php' method='post'>
+	        <div class='container-insciption-connexion'>
+	            <h2>Modifier mon MDP</h2>
+	            <?php
+	            //On verifie si une erreur à été envoyé.
+	            if(isset($_GET['err'])){
+	                //On cherche à quoi elle correspond et on l'affiche.
+	                switch($_GET['err']){
+	                    case "a_mdp_vide" :
+	                    echo "<p class='err'>Erreur : Ancien mot de passe vide.</p>";
+	                    break;
+
+	                    case "mdp_vide" :
+	                    echo "<p class='err'>Erreur : Nouveau mot de passe vide.</p>";
+	                    break;
+
+	                    case "mdp_retype_vide" :
+	                    echo "<p class='err'>Erreur : Confirmation nouveau mot de passe vide.</p>";
+	                    break;
+
+	                    case "non_identique" :
+	                    echo "<p class='err'>Erreur : Les nouveau mot de passe ne correspondent pas.</p>";
+	                    break;
+
+	                    case "a_mdp_faux" :
+	                    echo "<p class='err'>Erreur : Ancien mot de passe érroné.</p>";
+	                    break;
+
+	                    case "captcha_faux" :
+	                    echo "<p class='err'>Erreur : Le Captcha est érroné.</p>";
+	                    break;
+	                }
+	            }
+	            if(isset($_GET['succes'])){
+	                //On cherche à quoi elle correspond et on l'affiche.
+	                switch($_GET['succes']){
+	                    case "succes" :
+	                    echo "<p class='succes'>Succès: Mot de passe modifié !</p>";
+	                    break;
+	                }
+	            }
+	            ?>
+	            <div class='inputs'>
+	            		<p class='titre-form'>Ancien mot de passe</p>
+	                <label id="label-register-password" for="register-password">
+	                <input id='register-password' type='password' name='a_mdp' placeholder='ex : M0t_D3_P@55€'/>
+
+	                <p class='titre-form'>Nouveau mot de passe</p>
+	                <label id="label-register-password" for="register-password">
+	                <input id='register-password' type='password' name='mdp' placeholder='ex : N0Uv€@U_M0t_D3_P@55€'/>
+	                
+	                <p class='titre-form'>Confirmer nouveau Mot de passe</p>
+	                <label id="label-register-password-2" for="register-password-2">
+	                <input id='register-password-2' type='password' name='mdp_retype' placeholder='ex : N0Uv€@U_M0t_D3_P@55€'/>
+	                
+	                <p class='titre-form'>Captcha</p>
+	                <div class='captcha-img-desc'>
+	                    <img class='img-cptcha' src='captcha/img/captcha1.png' alt='image_captcha'>
+						<p class='captcha-desc-text'> Attention : Tous les caracères doivent être écrit en minuscules.</p>
+	                </div>
+					<audio controls>
+                            <source src="captcha/audio/captcha1.mp3" type="audio/mpeg">
+                    </audio>
+	                <label id="label-register-captcha" for="register-captcha">
+	                <input id='register-captcha' type='text' name='captcha' placeholder='ex : 7u5T5g3'/>
+	                <p><input type='submit' name='Envoyer' value='Modifier'></p>
+	                <p>Mot de passe oublié ? <a id='link-sincrip' href='404.php'>C'est par là !</a></p>
+	            </div>
+	        </div>
+	    </form>
+		</div>
+
+		<?php
+	  	//On inclus le footer de la page.
+	  	require("imports_html/footer.html");
+  	?>
+
+  </body>
+</html>
