87b71eb5bca52edad7845cc3b5780aaab1475871
Suban.S <sasikumarsuban2@gmail.com>
2022-12-10 13:48:08 +0100

    MAJ BD : Ajout colonnes pour page admin

--- dev_web/bd_SAE.sql
+++ dev_web/bd_SAE.sql
@@ -1,25 +1,28 @@
-create table users(
-    id_user int primary key auto_increment,
-	
-    login varchar(32) not null unique,
-    mdp varchar(255) not null check(length(mdp)>31),
-    type_user varchar(32) default 'user' check (type_user in ('admin','user','gestion'))
+
+CREATE TABLE `activiteconnexion` (
+  `id_connexion` int(11) NOT NULL,
+  `reussite` varchar(5) NOT NULL,
+  `mdp_tente` varchar(32) NOT NULL,
+  `log_tente` varchar(255) NOT NULL,
+  `date_horaire_tent` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `adr_ip` varchar(32) NOT NULL
 );
 
-create table activiteModule(
-	id_activite int primary key auto_increment,
-	
-	id_module int not null,
-    login varchar(32) not null unique,
-    date_horaire_utilis timestamp not null
+
+CREATE TABLE `activitemodule` (
+  `id_activite` int(11) NOT NULL,
+  `id_module` int(11) NOT NULL,
+  `login` varchar(32) NOT NULL,
+  `bool_utilisation` tinyint(1) NOT NULL DEFAULT 0
 );
 
-create table activiteConnexion(
-    id_connexion int primary key auto_increment,
-	
-    reussite varchar(5) not null,
-    mdp_tente varchar(32) not null,
-    log_tente varchar(255) not null,
-    date_horaire_tent timestamp not null,
-    adr_ip varchar(32) not null
+
+
+CREATE TABLE `users` (
+  `id_user` int(11) NOT NULL,
+  `login` varchar(32) NOT NULL,
+  `mdp` varchar(255) NOT NULL CHECK (octet_length(`mdp`) > 31),
+  `type_user` varchar(32) DEFAULT 'user' CHECK (`type_user` in ('admin','user','gestion')),
+  `nb_visites` varchar(100) NOT NULL DEFAULT '0'
 );
+
