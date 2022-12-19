f6e6772fcb7c3e31f2a0ec284b1078d90cadc06f
Suban.S <sasikumarsuban2@gmail.com>
2022-12-19 17:34:11 +0100

    MAJ BD : auto increment des ID + suppresion colonne inutile

--- dev_web/bd_SAE.sql
+++ dev_web/bd_SAE.sql
@@ -1,23 +1,21 @@
 
+
 CREATE TABLE `activiteconnexion` (
   `id_connexion` int(11) NOT NULL,
-  `reussite` varchar(5) NOT NULL,
+  `reussite` varchar(5) NOT NULL DEFAULT 'False',
   `mdp_tente` varchar(32) NOT NULL,
-  `log_tente` varchar(255) NOT NULL,
+  `login` varchar(255) NOT NULL,
   `date_horaire_tent` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
   `adr_ip` varchar(32) NOT NULL
-);
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 
 
 CREATE TABLE `activitemodule` (
   `id_activite` int(11) NOT NULL,
   `id_module` int(11) NOT NULL,
-  `login` varchar(32) NOT NULL,
-  `bool_utilisation` tinyint(1) NOT NULL DEFAULT 0
-);
-
-
+  `login` varchar(32) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 
 CREATE TABLE `historique_module2` (
@@ -29,8 +27,7 @@ CREATE TABLE `historique_module2` (
   `message` varchar(255) NOT NULL,
   `cle` int(11) NOT NULL,
   `resultat` varchar(255) NOT NULL
-);
-
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 
 CREATE TABLE `users` (
@@ -39,5 +36,32 @@ CREATE TABLE `users` (
   `mdp` varchar(255) NOT NULL CHECK (octet_length(`mdp`) > 31),
   `type_user` varchar(32) DEFAULT 'user' CHECK (`type_user` in ('admin','user','gestion')),
   `nb_visites` varchar(100) NOT NULL DEFAULT '0'
-);
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+
+ALTER TABLE `activiteconnexion`
+  ADD PRIMARY KEY (`id_connexion`);
+
+--
+-- Index pour la table `activitemodule`
+--
+ALTER TABLE `activitemodule`
+  ADD PRIMARY KEY (`id_activite`);
+
+--
+-- Index pour la table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id_user`);
+
+
+
+ALTER TABLE `activiteconnexion`
+  MODIFY `id_connexion` int(11) NOT NULL AUTO_INCREMENT;
+
+ALTER TABLE `activitemodule`
+  MODIFY `id_activite` int(11) NOT NULL AUTO_INCREMENT;
+
+ALTER TABLE `users`
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
