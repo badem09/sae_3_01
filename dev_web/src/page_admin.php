c6c776c944d5e7e0f71da6d28cf14b7db151807f
Guenfici Rayane <dracorixprod@gmail.com>
2022-12-24 16:11:31 +0100

    modif style + texte page + formalisme + correction

--- dev_web/src/page_admin.php
+++ dev_web/src/page_admin.php
@@ -38,80 +38,79 @@
 
         <div class="admin-main">
             <div class="recherche">
-                <h3> Choisiser une statistique</h3>
-                <form method="post">
+
+                <h2>Choisissez une statistique</h2>
+                <h3>Rechercher un utilisateur avec son login</h3>
+
+                <form method="post" id='post-admin-up'>
                     <input id='users' type='submit' name="users" value="Utilisateurs">
                     <input id='activitemodule' type='submit' name='activitemodule' value='Activite Module'>
                     <input id='activiteconnexion' type='submit' name='activiteconnexion' value='Connexion Echouée'>
                 </form>
 
-                <h3>Rechercher un utilisateur (login)</h3>
                 <form method="post">
                     <input id='researched' type='text' name="text"  placeholder="ex : demba404">
                     <input id='search' type='submit' name='search' value='Recherecher'>
                     <input id='clear' type='submit' name='retour' value='Retour'>
                 </form>
+
             </div>
 
             <div class="affichage">
-            <?php
-                //On inclus la configuration d'accès à la base de donnée avant de commencer.
-                require_once('config/config_bdd.php');
+                <?php
+                    //On inclus la configuration d'accès à la base de donnée avant de commencer.
+                    require_once('config/config_bdd.php');
 
 
-                if ( isset($_POST["users"]) ){  //si on appuie sur le bouton utilisateur
-                    $table = "users";           // alors on récupère la table
-                    setcookie("table", $table); //et on la met dans un cookie pour la stocker
-                    affichage($table);          // et on appel la fonction avec la table en paramètre
-                }
-                else if ( isset($_POST["activitemodule"]) ){
-                    $table = "activitemodule";
-                    setcookie("table", $table);
-                    affichage($table);
-                }
-                else if ( isset($_POST["activiteconnexion"]) ){
-                    $table = "activiteconnexion";
-                    setcookie("table", $table);
-                    affichage($table);
-                }
+                    if ( isset($_POST["users"]) ){  //si on appuie sur le bouton utilisateur
+                        $table = "users";           // alors on récupère la table
+                        setcookie("table", $table); //et on la met dans un cookie pour la stocker
+                        affichage($table);          // et on appel la fonction avec la table en paramètre
+                    }
+                    else if ( isset($_POST["activitemodule"]) ){
+                        $table = "activitemodule";
+                        setcookie("table", $table);
+                        affichage($table);
+                    }
+                    else if ( isset($_POST["activiteconnexion"]) ){
+                        $table = "activiteconnexion";
+                        setcookie("table", $table);
+                        affichage($table);
+                    }
 
-                //Si une recherche a été demandé.
-                if ((isset($_POST["text"], $_POST["search"]))) {
-                    recherche($_POST["text"], $_COOKIE["table"]);
+                    //Si une recherche a été demandé.
+                    if ((isset($_POST["text"], $_POST["search"]))) {
+                        recherche($_POST["text"], $_COOKIE["table"]);
 
-                //Si un retour à été demandé, on redirige vers la meme page pour actualiser et on ferme celle-ci.
-                } elseif (isset($_POST["retour"]) ){
-                    header("location:page_admin.php");
-                    die();
-                }
-
-            ?>
+                    //Si un retour à été demandé, on redirige vers la meme page pour actualiser et on ferme celle-ci.
+                    } elseif (isset($_POST["retour"]) ){
+                        header("location:page_admin.php");
+                        die();
+                    }
+                ?>
             </div>
         </div>
     
     </body>
 </html>
 
-
-
-
 <?php
 
-function affichage($table){ //affiche l'entierete de la table choisi selon le paramètre
+    function affichage($table){ //affiche l'entierete de la table choisi selon le paramètre
 
-    //configuration d'accès à la base de donnée avant de commencer.
-    $connexion=mysqli_connect("localhost","root","");
-    $bd=mysqli_select_db($connexion,"bd_sae");
+        //configuration d'accès à la base de donnée avant de commencer.
+        $connexion=mysqli_connect("localhost","root","");
+        $bd=mysqli_select_db($connexion,"bd_sae");
 
-    if ($table == "users") {
-        $requete1 = mysqli_query($connexion,"SELECT id_user, login, type_user, nb_visites from $table");
-    }
-    else {
-        $requete1 = mysqli_query($connexion,"SELECT * from $table");
-    }
+        if ($table == "users") {
+            $requete1 = mysqli_query($connexion,"SELECT id_user, login, type_user, nb_visites from $table");
+        }
+        else {
+            $requete1 = mysqli_query($connexion,"SELECT * from $table");
+        }
 
-    //On affiche la base d'un tableau.
-    echo "<table class='tab'>";
+        //On affiche la base d'un tableau.
+        echo "<table class='tab'>";
 
         //On affiche les titres du tableau selon la table sélectionné.
         if ($table == "users") {
@@ -132,24 +131,26 @@ function affichage($table){ //affiche l'entierete de la table choisi selon le pa
             }
             echo "</tr>";
         }
-    echo "</table>";
 
-}
+        echo "</table>";
 
-function recherche($texte,$table){ //affiche le contenu de la table selon les paramètres
-
-    //configuration d'accès à la base de donnée avant de commencer.
-    $connexion=mysqli_connect("localhost","root","");
-    $bd=mysqli_select_db($connexion,"bd_sae");
-
-    if ($table == "users") {
-        $requete = mysqli_query($connexion,"SELECT id_user, login, type_user, nb_visites from $table where login like '".$texte."%'");
     }
-    else {
-        $requete = mysqli_query($connexion, "SELECT * from $table where login like '".$texte."%'");
-    }
-    //On affiche la base d'un tableau.
-    echo "<table class='tab'>";
+
+
+    function recherche($texte,$table){//affiche le contenu de la table selon les paramètres
+
+        //configuration d'accès à la base de donnée avant de commencer.
+        $connexion=mysqli_connect("localhost","root","");
+        $bd=mysqli_select_db($connexion,"bd_sae");
+
+        if ($table == "users") {
+            $requete = mysqli_query($connexion,"SELECT id_user, login, type_user, nb_visites from $table where login like '".$texte."%'");
+        }
+        else {
+            $requete = mysqli_query($connexion, "SELECT * from $table where login like '".$texte."%'");
+        }
+        //On affiche la base d'un tableau.
+        echo "<table class='tab'>";
 
         //On affiche les titres du tableau selon la table sélectionné.
         if ($table == "users") {
@@ -170,5 +171,8 @@ function recherche($texte,$table){ //affiche le contenu de la table selon les pa
             }
             echo "</tr>";
         }
-    echo "</table>";
-}
+
+        echo "</table>";
+    }
+
+?>
