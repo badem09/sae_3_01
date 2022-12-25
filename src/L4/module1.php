dc4e6a89acbfebc2675bcddd7f6f940fbfba08b7
Guenfici Rayane <dracorixprod@gmail.com>
2022-12-26 00:03:19 +0100

    Ajout du livrable 4

---
+++ src/L4/module1.php
@@ -0,0 +1,256 @@
+<?php
+
+    //On regarde si une cession existe.
+    session_start();
+    //Si aucune cession existe, on renvois sur la page de connexion.
+    if(!isset($_SESSION['user'])) {
+       header('Location: connexion.php');
+    }
+ 
+?>
+
+<!doctype html>
+<html lang="fr">
+
+    <script type="text/javascript">
+        /*
+        logiciel libre sous licence MIT
+        auteur: Alain Busser
+        date: 27 mai 2013
+        source : https://irem.univ-reunion.fr/IMG/html/normales.html
+        */
+
+        var coef=Math.sqrt(2*Math.PI);
+
+        function arrondi(x,e){
+            var p10=Math.pow(10,e);
+            return(Math.round(p10*x)/p10);
+        }
+
+        function arrondi_inf(x,e){
+            var p10=Math.pow(10,e);
+            return(Math.floor(p10*x)/p10);
+        }
+
+        function arrondi_sup(x,e){
+            var p10=Math.pow(10,e);
+            return(Math.ceil(p10*x)/p10);
+        }
+
+
+        function phi(x){
+            return Math.exp(-x*x/2)/coef;
+        }
+
+        function erf(x){
+            var t=1/(1+0.3275911*x);
+            var ye=1.061405429;
+            ye=ye*t-1.453152027;
+            ye=ye*t+1.421413741;
+            ye=ye*t-0.284496736;
+            ye=ye*t+0.254829592;
+            ye*=t;
+            ye*=Math.exp(-x*x);
+            return (1-ye);
+        }
+
+        function Pi(x){
+            if(x<0){return(1-Pi(-x));} else {
+                if(x<100){
+                    return((1+erf(x/Math.SQRT2))/2);
+                } else {
+                    return(1);
+                }
+            }
+        }
+
+        function maj(){
+            mu=parseFloat(document.getElementById('esp').value);
+            sigma=Math.abs(parseFloat(document.getElementById('et').value));
+            Xmin=Math.max(mu-100*sigma,parseFloat(- document.getElementById('t').value));
+            Xmax=Math.min(mu+100*sigma,parseFloat(document.getElementById('t').value));
+            a=(Xmin-mu)/sigma;
+            b=(Xmax-mu)/sigma;
+            odg=1-Math.round(Math.log(8*sigma)/Math.LN10);
+            pdec=Math.pow(10,-odg);
+            remplir2();
+
+        }
+
+        function remplir2(){
+            var ctx2=document.getElementById('can2');
+            if (ctx2.getContext){
+                var ctx2=ctx2.getContext('2d');
+                ctx2.fillStyle="White";
+                ctx2.fillRect(0,0,400,240);
+                ctx2.fillStyle="Lightgreen";
+                ctx2.strokeStyle="Cyan";
+                ctx2.beginPath();
+                ctx2.moveTo(0,220);
+
+                for(x=0;x<=Math.round(200+50*b);x++){
+                    ctx2.lineTo(x,220-500*phi((x-200)/50));
+                }
+
+                ctx2.lineTo(200+50*b,220);
+                ctx2.lineTo(0,220);
+                ctx2.stroke();
+                ctx2.fill();
+
+                ctx2.strokeStyle="Red";
+                ctx2.beginPath();
+                ctx2.moveTo(0,220);
+
+                for(x=1;x<=400;x++){
+                    ctx2.lineTo(x,220-500*phi((x-200)/50));
+                }
+
+                ctx2.stroke();
+                ctx2.strokeStyle="Blue";
+                ctx2.fillStyle="Magenta";
+                ctx2.beginPath();
+
+                for(var xg=arrondi_sup(mu-4*sigma,odg);xg<arrondi_inf(mu+4*sigma,odg);xg=arrondi(xg+pdec,odg)){
+                    x=(xg-mu)/sigma;
+                    x=x*50+200;
+                    ctx2.moveTo(x,220);
+                    ctx2.lineTo(x,225);
+                    ctx2.fillText(xg.toString(),x-5,235);
+                }
+
+                ctx2.moveTo(0,220);
+                ctx2.lineTo(400,220);
+                ctx2.stroke();
+            }
+        }
+    </script>
+  
+
+  <?php
+    //On inclus le header de la page.
+    require("imports_html/head.html");
+  ?>
+  
+  <body>
+		
+        <?php
+            //On inclus la barre de navigation.
+            require("imports_html/nav_bar.html");
+        ?>
+
+        <div class="entete">
+            <h1>X Calculator</h1>
+            <h2>Module de probabilité.</h2>
+            <br>
+            <p class="pacc_mod_pres">Vous pouvez calculer P(X < t) en saisissant la valeur de m, de σ et t (à 10^-5 près).</p>
+        </div>
+
+        <div class='container-module-parent'>
+            <form name="form_module1" action='' method='post'>
+                <div class='container-module'>
+                    <div class='container-form'>
+                        <h2>Module de probabilité.</h2>
+                        <p>Calcul d'une probabilité à l'aide d'une loi normale</p>
+
+                        <div class='inputs-module'>
+
+                            <p class="titre-form-module-proba">Méthode de calcule :</p>
+                            <select name="choix_methode" id="choix_methode">
+                                <option  selected disabled hidden>Choisissez une méthode</option>
+                                <option value="rectangles_gauches.py">Méthode rectangles gauches</option>
+                                <option value="rectangles_droites.py">Méthode rectangles droits</option>
+                                <option value="rectangles_medians.py">Méthode rectangles medians</option>
+                                <option value="methode_trapezes.py">Méthode Trapèzes</option>
+                                <option value="methode_simpson.py">Méthode Simpson</option>
+                            </select>
+
+                            <p class="titre-form-module-proba">Valeur de µ :</p>
+                            <input aria-label="esp" type="text" name="esp" id="esp" placeholder="ex : 1" value="<?php echo isset($_POST['esp']) ? $_POST['esp'] : '' ?>"/>
+
+                            <p class="titre-form-module-proba">Valeur de σ :</p>
+                            <input aria-label="et" type="text" name="et" id="et" placeholder="ex : 1" value="<?php echo isset($_POST['et']) ? $_POST['et'] : '' ?>"/>
+
+                            <p class="titre-form-module-proba">Valeur de t :</p>
+                            <input aria-label="t" type="text" name="t" id ="t" placeholder="ex : 1" value="<?php echo isset($_POST['t']) ? $_POST['t'] : '' ?>"/>
+                        </div>
+                        <input id="input-module-send" name='submit' type="submit" value="Calculer"></input>
+                    </div>
+                    <div class="vertical-line"></div>
+                    <div class='container-resultat'>
+                        <h2>Résultat</h2>
+                        <?php
+                            //Si le bouton calculer est cliqué on continus.
+                            if(isset($_POST['submit'])){
+                                //Si le champ "choix_methode" n'est pas vide on continus.
+                                if(!empty($_POST['choix_methode'])){
+                                    //Si le champ "esp" n'est pas vide on continus.
+                                    if(!empty($_POST['esp'])){
+                                        //Si le champ "et" n'est pas vide on continus.
+                                        if(!empty($_POST['et'])){
+                                            //Si le champ "t" n'est pas vide on continus.
+                                            if(!empty($_POST['t'])){
+
+                                                #Insertion pour avoir les statistiques d'utilisations
+
+                                                //On inclus la configuration de la base de données.
+                                                require_once('config/config_bdd.php');
+                                                //On prépare la requet qui vas inserer dans la table "activitemodule", le numéro du module et l'utilisateur.
+                                                $requete="INSERT INTO activitemodule (id_module, login) VALUES  (1, '".$_SESSION["user"]["login"]."')";
+                                                //On execute la requete.
+                                                $requete2 = mysqli_query($connexion, $requete);
+
+                                                #On récupere les entrées.
+                                                $esp = $_POST["esp"];
+                                                $et = $_POST["et"] ;
+                                                $t = $_POST["t"];
+                                                #On vérifie si elle sont bien des chiffres.
+                                                if (!(is_numeric($esp) && is_numeric($et) && is_numeric($t))){
+                                                    //Si non on renvois une erreur.
+                                                    echo "<p class='err'> Erreur : Un (ou plusieurs) paramêtre n'est pas au bon format.</p>";
+                                                //On vérifie que "et" n'est pas null.
+                                                } else if ($et <= 0){
+                                                    //Si non on renvois une erreur.
+                                                    echo "<p class='err'> La valeur de σ ne peux pas être inférieure ou égale à 0.</p>";
+                                                //Dans le cas ou tout est bon on calcule et on affiche les resultats.
+                                                } else {
+                                                $fonction = $_POST["choix_methode"] ;
+                                                #Préparation de la commande.
+                                                $command = "python python_module1/$fonction $esp $et $t "; 
+                                                #Execution de la commande et on récupere le resultat.
+                                                $result = exec($command); 
+                                                #On affiche le résultat.
+                                                echo "<h1>".$result."</h1>";
+                                                #On envoie le graphique.
+                                                echo '<h4>Représentation graphique</h4>
+                                                    <canvas id="can2" width="400" height="240"></canvas>
+                                                    <script type="text/javascript">maj();</script>';
+                                                }
+
+                                            }else{
+                                                //Si µ est vide, on affiche une erreur.
+                                                echo "<p class='err'> Erreur : Le champ \"t\" est vide.</p>";
+                                            }
+                                        }else{
+                                            //Si σ est vide, on affiche une erreur.
+                                            echo "<p class='err'> Erreur : Le champ \"σ\" est vide.</p>";
+                                        }
+                                    }else{
+                                        //Si t est vide, on affiche une erreur.
+                                        echo "<p class='err'> Erreur : Le champ \"µ\" est vide.</p>";
+                                    }
+                                }else{
+                                    //Si la méthode de calcule n'est pas choisis, on affiche une erreur.
+                                    echo "<p class='err'> Erreur : Vous n'avez pas choisis la methode.</p>";
+                                }
+                            }
+
+                        ?>
+                    </div>
+                </div>
+            </form>
+        </div>
+    <?php
+        //On inclus le footer de la page.
+        require("imports_html/footer.html");
+    ?>
+</html>
---
+++ src/L4/python_module2/rc4.py
@@ -0,0 +1,84 @@
+import sys 
+
+def RC4(action,key, message):
+    """Algorithme RC4 : Chiffre ou déchiffre un message selon une clé
+
+    Entrée :
+            action (str) : 'c' (chiffrement) ou 'd' (déchiffrement)
+            key (str) : clé
+            message (str) : message
+        
+    Sortie : (str)
+            si chiffrement : Chaîne de caractère de nombres héxadécimaux
+            si déchiffrement : Chaine de caractère contenant le message
+    """
+    
+    key = [ord(c) for c in key] # valeur ascii des lettre de la clé
+    if action == "c" : 
+        message = [ord(c) for c in message] # valeur ascii des lettre du message
+    else :
+        message = hexa_to_ten(message) # valeur décimale de la chaine de nombres héxadécimaux
+
+    # Initialiser la suite chiffrante
+    suite = list(range(256))
+    j = 0
+    for i in range(256):
+        j = (j + suite[i] + key[i % len(key)]) % 256
+        suite[i], suite[j] = suite[j], suite[i]
+
+    # Appliquer l'algorithme RC4 au message (cf PRGA(S) dans cours)
+    result = []
+    i = j = 0
+    for lettre in message:
+        i = (i + 1) % 256
+        j = (j + suite[i]) % 256
+        suite[i], suite[j] = suite[j], suite[i]
+        result.append(lettre ^ suite[(suite[i] + suite[j]) % 256]) # ^ applique l'opérateur logique xor 
+
+    if action == "c":
+        return result
+    else : #si déchiffrement
+        return ''.join([chr(e) for e in result])
+        
+
+def ten_to_hexa(liste):
+    """Convertit une liste de nombres décimaux (base 10) en 
+    une chaîne de charactères de nombres héxadécimaux (base 16) 
+
+    Entrée : 
+            (list) : Nombres décimaux 
+    Sortie :
+            str (str) : Nombres héxadécimaux
+
+    """    
+    liste = [hex(e)[2:].upper() if len(str(hex(e)))>3 else "0" + hex(e)[2:].upper() for e in liste]
+    return " ".join(liste)
+
+def hexa_to_ten(str):
+    """Convertit une chaîne de charactères de nombres héxadéciamaux 
+    (base 16) liste de nombres décimaux (base 10)
+
+    Entrée : 
+            str (str) : Nombres héxadécimaux
+
+    Sortie :
+            (list) : Nombres décimaux 
+    """
+    liste = str.split(" ")
+    return [int('0x' + cara , 0) for cara in liste]
+
+try:
+    action = sys.argv[1]
+    data = sys.argv[2]
+    key = sys.argv[3]
+
+    res = RC4(action,key,data)
+
+    if action == "c":
+        print(ten_to_hexa(res))
+    else: 
+        print(res)
+
+except:
+    print("Le message ne possede pas le bon format")
+
