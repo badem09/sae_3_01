53bee2f65e517395ec963a3c3374f43bde9cd5cc
Demba BA <badembajob@gmail.com>
2023-01-04 22:46:14 +0100

    Suppression de la col id module et ajout de bool selon si rc4 ou WPE (table historique module 2)

--- dev_web/bd_SAE.sql
+++ dev_web/bd_SAE.sql
@@ -20,13 +20,15 @@ CREATE TABLE `activitemodule` (
 
 CREATE TABLE `historique_module2` (
   `id_historique` int(11) NOT NULL,
-  `id_module` int(11) NOT NULL DEFAULT 2,
   `login` varchar(255) NOT NULL,
   `bool_chiffrement` tinyint(1) NOT NULL DEFAULT 0,
   `bool_dechiffrement` tinyint(1) NOT NULL DEFAULT 0,
   `message` varchar(255) NOT NULL,
   `cle` varchar(255) NOT NULL,
-  `resultat` varchar(255) NOT NULL
+  `resultat` varchar(255) NOT NULL,
+  `bool_rc4` tinyint(1) NOT NULL DEFAULT 0,
+  `bool_webp` tinyint(1) NOT NULL DEFAULT 0
+
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 
