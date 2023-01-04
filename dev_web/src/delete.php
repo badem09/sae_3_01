8a1a884ba19e794b465cdc85f8fb6bef90997c3c
Demba BA <badembajob@gmail.com>
2023-01-05 00:28:01 +0100

    test protection branche

--- dev_web/src/delete.php
+++ dev_web/src/delete.php
@@ -20,8 +20,6 @@ $requete4 = mysqli_query($connexion, "DELETE FROM historique_module1 where login
                                         (SELECT login FROM users where id_user = '".$supp."')");
 
 
-
-
 // Redirection vers la page d'affichage des enregistrements
 header('Location: page_admin.php');
 exit;
