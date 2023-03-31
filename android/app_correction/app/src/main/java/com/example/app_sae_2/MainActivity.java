6e70e7a79973c6e02be6a615127732f0930943b6
android <android@dinfo69.dinfo.iut-velizy.uvsq.fr>
2023-03-31 11:55:30 +0200

    Page utilisation module 1

--- android/app_correction/app/src/main/java/com/example/app_sae_2/MainActivity.java
+++ android/app_correction/app/src/main/java/com/example/app_sae_2/MainActivity.java
@@ -13,7 +13,6 @@ import android.widget.Button;
 public class MainActivity extends AppCompatActivity {
     Button bt1;
     Button bt2;
-    Button bt3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,16 +21,29 @@ public class MainActivity extends AppCompatActivity {
         bt1 = findViewById(R.id.button);
         bt1.setOnClickListener(this::ActionBouton1);
     }
+
+
     //bouton pour aller page 2
-    public void ActionBouton1(View view) {
+    public void ActionBouton1(View view) { //module1 explication
         setContentView(R.layout.activity_main2);
         bt2 = findViewById(R.id.button2);
+        Button bt3 = findViewById(R.id.button3);
+        bt2.setOnClickListener(this::ModuleProba_Calcul);
+        bt3.setOnClickListener(this::Mdoule2_Explication);
+
     }
+
+    private void ModuleProba_Calcul(View view) {
+        setContentView(R.layout.activity_main4);
+        Button bt_module2  = findViewById(R.id.button3);
+        bt_module2.setOnClickListener(this::Mdoule2_Explication);
+    }
+
     //bouton essayer du module 1
     public void ActionBouton2(View view) {
     }
     //bouton pour aller page 3
-    public void ActionBouton3(View view) {
+    public void Mdoule2_Explication(View view) { //module2 explication
         setContentView(R.layout.activity_main3);
     }
     //bouton de la page 2 pr revenir page 1
