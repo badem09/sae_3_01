eec2d0fb1e40da4183b4cd5da7c0bfe190078eb1
Demba BA <badembajob@gmail.com>
2023-05-11 22:04:55 +0200

    Gestion des erreurs + correction bug input float

--- android/app_correction/app/src/main/java/com/example/app_sae_2/MainActivity.java
+++ android/app_correction/app/src/main/java/com/example/app_sae_2/MainActivity.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -53,22 +54,24 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.layout_inscription);
     }
 
-    public void executeModule2(View view) throws Exception {
-        Spinner spinnerAction = (Spinner) findViewById(R.id.spinnerAction);
+    public void executeModule2(View view){
+        Spinner spinnerAction = findViewById(R.id.spinnerAction);
         String action = spinnerAction.getSelectedItem().toString();
 
-        Spinner spinnerMethode = (Spinner) findViewById(R.id.spinnerMethode);
+        Spinner spinnerMethode = findViewById(R.id.spinnerMethode);
         String methode = spinnerMethode.getSelectedItem().toString();
-
-        System.out.println(action+ "  " +methode);
-
 
         EditText edkey = findViewById(R.id.editTextClef);
         EditText edmessage = findViewById(R.id.editTextMessage);
         String key = edkey.getText().toString();
         String message = edmessage.getText().toString();
+        String res;
 
-        String res = "";
+        String choixDeBase = getResources().getStringArray(R.array.spinnerMethodeModule1)[0];
+        if (key.equals("") || message.equals("") || methode.equals(choixDeBase)){
+            Toast.makeText(getApplicationContext(), R.string.erreurChoixParametres, Toast.LENGTH_SHORT).show();
+            return ;
+        }
 
         try {
             if (methode.equals("RC4")){
@@ -86,44 +89,59 @@ public class MainActivity extends AppCompatActivity {
         resultat.setText(res);
     }
 
-    public void executeModule1(View view) throws Exception {
+    public void executeModule1(View view){
         EditText edtT = findViewById(R.id.editTextT);
         EditText edtESP = findViewById(R.id.editTextESP);
         EditText edtET = findViewById(R.id.editTextET);
         TextView tvRes = findViewById(R.id.textViewResultatModule1);
+        Spinner spinnerChoix = findViewById(R.id.spinnerMethodeModule1);
+        String choix = spinnerChoix.getSelectedItem().toString();
+
+        if (edtESP.getText().toString().equals("") || edtET.getText().toString().equals("") ||
+                edtT.getText().toString().equals("")){
+            Toast.makeText(getApplicationContext(), R.string.erreurChoixParametres, Toast.LENGTH_SHORT).show();
+            tvRes.setText(R.string.erreur);
+            return ;
+        }
 
         try {
-
-            double et = Integer.parseInt(edtET.getText().toString());
-            double esp = Integer.parseInt(edtESP.getText().toString());
-            double t = Integer.parseInt(edtT.getText().toString());
-
-            Spinner spinnerChoix = findViewById(R.id.spinnerMethodeModule1);
-            String choix = spinnerChoix.getSelectedItem().toString();
-
+            double et = Float.parseFloat(edtET.getText().toString());
+            double esp = Float.parseFloat(edtESP.getText().toString());
+            double t = Float.parseFloat(edtT.getText().toString());
             double res = 0;
-            System.out.println(choix);
-            switch (choix) {
-                case "Choisir Une méthode" : throw new Exception();
-                case "Rectangle Gauche":
+            String[] arrayMethode = getResources().getStringArray(R.array.spinnerMethodeModule1);
+            int index = -1;
+            for (int i = 0; i< arrayMethode.length; i++){
+                if (choix.equals(arrayMethode[i])){
+                    index = i;
+                }
+            }
+            switch (index) {
+                case 0 : // "Choisir Une méthode" :
+                    Toast.makeText(getApplicationContext(), R.string.erreurChoixModule, Toast.LENGTH_SHORT).show();
+                    tvRes.setText(R.string.erreur);
+                    return ;
+                case 1 : // "Rectangle Gauche":
                     res = Module1.methode_rectangle_gauche(esp, et, t, 1000);
-                case "Rectangle Droit":
+                    break;
+                case 2 : // "Rectangle Droit":
                     res = Module1.methode_rectangle_droite(esp, et, t, 1000);
-                case "Rectangle Median":
+                    break;
+                case 3 : // "Rectangle Median":
                     res = Module1.methode_rectangle_medians(esp, et, t, 1000);
-                case "Méthode des Trapèzes":
+                    break;
+                case 4 : // "Méthode des Trapèzes":
                     res = Module1.methode_trapeze(esp, et, t, 1000);
-                case "Méthode de Simpsons":
+                    break;
+                case 5  ://"Méthode de Simpsons":
                     res = Module1.methode_simpson(esp, et, t, 1000);
+                    break;
             }
             tvRes.setText(String.valueOf(res));
         }
         catch (Exception e){
             e.printStackTrace();
-            tvRes.setText("Erreur");
+            tvRes.setText(R.string.erreur);
         }
     }
-
-
-
 }
