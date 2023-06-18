42de2184cc49c6eb4acc3c3c25084bba56bba031
Demba BA <badembajob@gmail.com>
2023-06-18 18:26:58 +0200

    Suppr génération du fichier csv + correction format date

--- src/python_module3/WebScrapping.py
+++ src/python_module3/WebScrapping.py
@@ -5,6 +5,7 @@ from joblib import load
 import sklearn
 import json
 import sys
+from datetime import datetime
 
 
 def moduleML(url: str) -> list[str]:
@@ -18,39 +19,34 @@ def moduleML(url: str) -> list[str]:
     div_time = soup.find_all('time',{"class": "o-date"})
     div_article = soup.find_all("div", {"class": "o-teaser--article"})
     div_title_article = [div.find("div", {"class": "o-teaser__heading"}) for div in div_article]
+    titles_articles = [x.getText() for x in div_title_article]
 
     liste_time = [div["datetime"] for div in div_time]
 
-    title = "News_Financieres;"
-
-    with open('data_WebScrapping.csv', 'w', encoding='utf-8') as csv_file:
-        csv_file.write(title + '\n')
-        for x in div_title_article:
-            csv_file.write(x.getText() + ";" + '\n')
-
-    df = pd.read_csv("data_WebScrapping.csv", sep=';')
-    df = df.drop(columns="Unnamed: 1")
-    model = load('model.joblib')
+    df = pd.DataFrame(titles_articles,columns=["News_Financieres"])
+    model = load('python_module3/model.joblib')
     predictions = model.predict(df.News_Financieres)
 
-    d = {}
+    dict_predictions = {}
     for title, prediction, time in zip(df.News_Financieres, predictions, liste_time):
-        time = time.split(".")[0].replace("T"," ")
-        d[title] = prediction, time
-    return json.dumps(d)
+        str_time = time.split(".")[0].replace("T"," ")
+        time = datetime. strptime(str_time[:19], '%Y-%m-%d %H:%M:%S')
+        dict_predictions[title] = prediction, time.strftime("%d/%m/%Y %H:%M:%S")
+    return json.dumps(dict_predictions)
 
 def prediction_phrase(phrase:str) -> str:
-    model = load('model.joblib')
+    model = load('python_module3/model.joblib')
     prediction = model.predict([phrase])
     return prediction[0]
 
 
 if __name__ == "__main__":
+
     if len(sys.argv)>1:
         print(sys.argv[1])
         print(prediction_phrase(sys.argv[1]))
     else:
         predictions_json = moduleML('https://www.ft.com/global-economy')
-        with open("predictions.json", "w") as outfile:
+        with open("python_module3/predictions.json", "w") as outfile:
             outfile.write(predictions_json)
             
